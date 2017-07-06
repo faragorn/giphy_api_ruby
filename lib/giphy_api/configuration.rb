52af95e17be8b1fc26c774747fc1525e334de126
Farrukh Abdulkadyrov <farrukhabdul@gmail.com>
2017-07-06 03:32:49 -0400

    Add api_prefix config option

--- lib/giphy_api/configuration.rb
+++ lib/giphy_api/configuration.rb
@@ -1,9 +1,10 @@
 module GiphyAPI
   class Configuration
-    attr_accessor :api_key
+    attr_accessor :api_key, :api_prefix
 
     def initialize
       self.api_key = ENV['GIPHY_API_KEY']
+      self.api_prefix = 'v1'
     end
   end
 end
